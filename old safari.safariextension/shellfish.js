6d18bf4ce0e1013688c280401324377d4187738a
tobinibot <tobinibot@gmail.com>
2015-08-24 05:49:21 -0400

    moved new Safari into place

--- old safari.safariextension/shellfish.js
+++ old safari.safariextension/shellfish.js
@@ -9,13 +9,13 @@ function handleBeforeLoadEvent(event) {
     // synchronous message event, so it blocks while waiting for an answer.
     // If canLoad returns true return early, allowing the load to occur.
     if (element.nodeName === "IFRAME" || element.nodeName === "SCRIPT") {
-		// console.log('Checking ' + element.nodeName + ' ' + event.url);
+		console.log('Checking ' + element.nodeName + ' ' + event.url);
 		
 		if (safari.self.tab.canLoad(event, { url: event.url, nodeName: element.nodeName, host: window.location.hostname }))
 	        return;
 
 	    // Since the load should be blocked, call preventDefault on the event to block it.
-	    // console.log('Blocking ' + element.nodeName + ' ' + event.url);
+	    console.log('Blocking ' + element.nodeName + ' ' + event.url);
 	    event.preventDefault();
 	}
 }
