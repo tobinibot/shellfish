46307e0b4fea902403b5e6f11fac60ee0d66aa05
tobinibot <tobinibot@gmail.com>
2015-09-05 10:00:06 -0400

    reload the blocked list when the app starts

--- ios/Shellfish/ViewController.swift
+++ ios/Shellfish/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		SFContentBlockerManager.reloadContentBlockerWithIdentifier("com.open-bits.Shellfish.Shellfish-Content-Blocker", completionHandler: nil)
+		
 	}
 
 	override func didReceiveMemoryWarning() {
